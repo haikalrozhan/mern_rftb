3076c31bd81da59a80a2f6026228e4ac6d976f85
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-07-15 16:50:18 +0800

    Error exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(201).json({ mssg: "Welcome to support desk API" });
@@ -10,6 +14,8 @@ app.get("/", (req, res) => {
 
 // routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
